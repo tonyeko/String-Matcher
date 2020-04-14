815b75494d333c38ad3ce3bdd3fe37f310240cf9
tony <tonyeko@users.noreply.github.com>
2020-04-14 19:01:57 +0700

    folder uploader

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
 
+UPLOAD_FOLDER = "../test"
+
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key'
+    UPLOAD_FOLDER = UPLOAD_FOLDER
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024 #16MB
